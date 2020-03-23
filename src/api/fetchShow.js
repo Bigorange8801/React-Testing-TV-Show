1a9a274efff64e8132a3565f7736d9433866b08d
John <cjd8801@gmail.com>
2020-03-23 18:08:02 -0500

    added api folder and fetchshow

---
+++ src/api/fetchShow.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const fetchShow = async () =>{
+
+ useEffect(() => {
+    const fetchShow = () => {
+      axios
+        .get(
+          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+        )
+        .then(res => {
+          setShow(res.data);
+          setSeasons(formatSeasons(res.data._embedded.episodes));
+        });
+    };
+    fetchShow();
+  }, []);
+}
